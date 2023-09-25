b5e3f65e2e2722d6bffdf5f3a67c24751e0f969e
1chooo <hugo970217@gmail.com>
2023-09-25 10:46:14 +0800

    add: week01, MT1051

--- MT1051/go.mod
+++ MT1051/go.mod
@@ -1,3 +1,3 @@
-module MT1051
+module github.com/1chooo/23-fall-tutor/MT1051
 
 go 1.21.0
--- MT1051/week03/warm_up/find_root.py
+++ MT1051/week03/warm_up/find_root.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 Date: 2023/09/25
-HW: 05
+Warm up
 Author: 林群賀
 Student Number: 109601003
 """
 
 import math
+from typing import Any
 
 def find_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
@@ -40,3 +41,33 @@ if __name__ == "__main__":
             print(roots)
     except ValueError:
         print("Invalid input. Please enter valid numerical values for a, b, and c.")
+
+class FindRoot:
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        pass
+
+    def __init__(self) -> None:
+        pass
+
+    def get_parameters(self,) -> tuple:
+        a = float(input("Enter the value of a: "))
+        b = float(input("Enter the value of b: "))
+        c = float(input("Enter the value of c: "))
+
+        return a, b, c
+    
+    def get_root(self, a, b, c):
+        discriminant = b ** 2 - 4 * a * c
+    
+        if discriminant > 0:
+            root1 = (-b + math.sqrt(discriminant)) / (2 * a)
+            root2 = (-b - math.sqrt(discriminant)) / (2 * a)
+            return (
+                round(root1, 2), 
+                round(root2, 2)
+            )
+        elif discriminant == 0:
+            root1 = -b / (2 * a)
+            return round(root1, 2)
+        else:
+            return "No real roots"
