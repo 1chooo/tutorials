c6e4afde7ec9607727ccfb92ff6d007db2dd3388
1chooo <hugo970217@gmail.com>
2023-10-15 20:08:53 +0800

    add: week04 materials

---
+++ courses/course04/main.py
@@ -0,0 +1,40 @@
+print("Hello", "World", sep="+++")
+
+name = "Hugo"
+age = 18
+print(f"Hello, my name is {name} and I am {age} years old.")
+print("Hello, my name is", name, "and I am", age, "years old.")
+
+word = "Hello World"
+print(word)
+
+prices = [1, 2, 3, 4, 5]
+print(prices)
+
+# hello = 1
+# world = 2
+
+for i in range(0, 100, 1):
+    print(i, end=" ")
+print()
+for s in "hello":
+    print(s)
+
+prices = [100, 200, 300, 400, 500]
+for i in prices:
+    print(i)
+
+[0, 1, 2, 3, 4]
+
+for i in range(0, 5, 1):
+    print(i)
+
+for i in range(0, 5, 1):
+    print(prices[i])
+
+for i in range(0, 5, 2):
+    print(prices[i])
+
+
+num = input("jhrlksjdfhglkjdfhgnlkjsdg")
+print(num)
