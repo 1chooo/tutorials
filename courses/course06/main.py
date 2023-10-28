47807cb83cdeb776b07078b2c661b79489c80105
1chooo <hugo970217@gmail.com>
2023-10-28 15:16:22 +0800

    add: week06 materials

--- courses/course06/main.py
+++ courses/course06/main.py
@@ -3,3 +3,14 @@ for i in range(1, row + 1):
     for j in range(1, i + 1):
         print(j, end=' ')
     print()
+
+height = 7
+num = 1
+
+for i in range(1, height + 1):
+    row = ''
+    for j in range(i):
+        row += str(num) + ' '
+        num += 1
+    print(row.center(2 * height - 1))
+
